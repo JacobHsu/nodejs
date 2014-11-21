26013de403e872c869641fbc141a0e405973b924
Jacob Hsu <jacobhsu@dopod.com>
2014-11-21 15:46:33 +0800

    url parse fileName

--- url/index.js
+++ url/index.js
@@ -2,4 +2,7 @@ var url = require("url");
 var fileurl='http://upload/video/085dda8c813c23b9/f512f29f0133.mp4';
 var videoHash = url.parse(fileurl).pathname.split('/').slice(-2).shift(); //085dda8c813c23b9
 console.log(videoHash);
+var fileName = url.parse(fileurl).pathname.split('/').pop().split('.').shift();
+// [ '', 'video', '085dda8c813c23b9', 'f512f29f0133.mp4' ] -> f512f29f0133.mp4 -> [ 'f512f29f0133',  'mp4' ] -> f512f29f0133
+console.log(fileName);
 
