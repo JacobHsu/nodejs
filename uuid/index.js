a69f20052dfb9cd88984c10b2fb70a49ce08630d
Jacob Hsu <jacobhsu@dopod.com>
2016-03-30 13:37:29 +0800

    remove old projects

--- uuid/index.js
+++
@@ -1,7 +0,0 @@
-//NPM Note: https://www.npmjs.org/package/node-uuid
-
-var uuid = require('node-uuid');
-
-// Generate a v1 (time-based) id
-var id= uuid.v1(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a'
-console.log(id);
