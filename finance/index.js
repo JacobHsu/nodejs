b46457faabfe90eacca00751f9c987ed332e0c55
Jacob Hsu <jacob.hsu.tw@gmail.com>
2020-05-17 23:31:28 +0800

    etf

--- finance/index.js
+++ finance/index.js
@@ -12,18 +12,20 @@ var historical = function (symbol, from, to) {
     const quotesMap = quotes.map( obj => {
       return unwrap(obj)
     });
-    const arrDate = quotesMap.map( obj =>  moment(obj.date).format("yyyy-MM-DD"));
-    arrDate.unshift('x');
-    console.log(arrDate);
+    // const arrDate = quotesMap.map( obj =>  moment(obj.date).format("yyyy-MM-DD"));
+    // arrDate.unshift('x');
+    // console.log(arrDate);
     
     const arrClose = quotesMap.map( obj => obj.close.toFixed(2));
     arrClose.unshift(quotes[0].symbol);
     console.log(arrClose);
     // https://c3js.org/samples/axes_x_tick_count.html
-
+    return arrClose
   });
 }
 
-historical('vt', '2020-05-11', '2020-05-15')
-historical('vti', '2020-05-11', '2020-05-15')
+// historical('vt', '2020-05-11', '2020-05-15')
+// historical('vti', '2020-05-11', '2020-05-15')
 
+const arrStock = ['VT','VTI','EWJ','EWY','EWT','EWH','EWS']
+arrStock.map( stock => { historical(stock, '2020-05-11', '2020-05-15')  } );
