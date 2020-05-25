fd4edd9efd0ef713421d841dc56194705ec04142
jacobhsu <jacob.hsu.tw@gmail.com>
2020-05-25 10:36:43 +0800

    yahoo-finance a week

--- finance/index.js
+++ finance/index.js
@@ -12,10 +12,13 @@ var historical = function (symbol, from, to) {
     const quotesMap = quotes.map( obj => {
       return unwrap(obj)
     });
-    // const arrDate = quotesMap.map( obj =>  moment(obj.date).format("yyyy-MM-DD"));
-    // arrDate.unshift('x');
-    // console.log(arrDate);
-    
+
+    if(symbol === 'VT' ) {
+      const arrDate = quotesMap.map( obj =>  moment(obj.date).format("yyyy-MM-DD"));
+      arrDate.unshift('x');
+      console.log(arrDate);
+    }
+
     const arrClose = quotesMap.map( obj => obj.close.toFixed(2));
     arrClose.unshift(quotes[0].symbol);
     console.log(arrClose);
@@ -27,8 +30,20 @@ var historical = function (symbol, from, to) {
 // historical('vt', '2020-05-11', '2020-05-15')
 
 
-const arrStock = ['VT','VTI','EWJ','EWY','EWT','EWH','EWS']
+let arrStock = ['VT','VTI','EWJ','EWY','EWT','EWH','EWS']
 
-for(let stock of arrStock){
-  historical(stock, '2020-05-11', '2020-05-15') 
+// for(let stock of arrStock){
+//   historical(stock, '2020-05-11', '2020-05-15') 
+// }
+
+const d = new Date();
+const today = d.toISOString().substring(0, 10);
+//console.log(today) //  "yyyy-mm-dd" format. 2020-05-25
+
+let sevenDaysAgo = d.setDate(d.getDate() - 7);
+sevenDaysAgo = new Date(sevenDaysAgo).toISOString().substring(0, 10);;
+//console.log(sevenDaysAgo) // 2020-05-18
+
+for(let i = 0; i < arrStock.length; i++) {
+  historical(arrStock[i], sevenDaysAgo, today) 
 }
